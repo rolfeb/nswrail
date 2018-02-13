f5846aa6d0a0f48b980121a1ae9326698e8f20ab
Rolfe Bozier <rolfe@pobox.com>
2018-02-13 19:56:36 +1100

    rtially converted search to use latte.

--- public_html/search/text.php
+++ public_html/search/text.php
@@ -1,30 +1,50 @@
 <?php
 
-require_once "site.inc";
+require "site.inc";
 
-$title = "Text Search";
+#
+# Highlight the position[s] of each keyword in the context string. The result
+# will necessarily be rendered by latte without escaping, so we need to be
+# careful:
+#   1. The text string is untrusted.
+#   2. The keyword is untrusted.
+#
+function highlight_keywords($text, $keywords)
+{
+    foreach ($keywords as $kw) {
+        $kw_len = strlen($kw);
+        list($done, $remaining) = ['', $text];
 
-$t = new HTML_Template_ITX(".");
-$t->loadTemplateFile("text.tpl");
+        while (TRUE) {
+            $pos = stripos($remaining, $kw);
+            if ($pos !== FALSE) {
+                # match -> escape the prefix, highlight the kw, and continue
+                $done .=
+                    htmlspecialchars(substr($remaining, 0, $pos))
+                    . "<span class=\"caption-highlight\">$kw</span>";
+                $remaining = 
+                    substr($remaining, $pos + $kw_len);
+            } else {
+                # no match -> escape the remaining text, and break
+                $text = $done . htmlspecialchars($remaining);
+                break;
+            }
+        }
+    }
 
-$searchmode = quote_external(get_post("searchmode"));
+    return $text;
+}
 
-if ($searchmode)
-    perform_search($t);
+function sort_by_location($a, $b)
+{
+    if ($a['name'] == $b['name']) {
+        return 0;
+    } else {
+        return $a['name'] < $b['name'] ? -1 : 1;
+    }
+}
 
-$t->setCurrentBlock("CONTENT");
-$t->setVariable("TITLE", $title);
-$t->parseCurrentBlock();
-
-display_page($title, $t->get("CONTENT"),
-    array(
-        'HEAD-EXTRA'    => implode(file('text.hdr'), ""),
-    )
-);
-
-return;
-
-function perform_search(&$t)
+function perform_search($tp)
 {
     global $db;
 
@@ -34,26 +54,28 @@ function perform_search(&$t)
     $match_locndesc = quote_external(get_post("matchlocndesc"));
     $match_photo = quote_external(get_post("matchphotos"));
 
-    $results = array();
+    $rows = [];
 
-    if ($keywords)
-    {
-        $keyword_list = explode(" ", $keywords);
+    if ($keywords) {
+        $keyword_list = explode(" ", strtolower($keywords));
 
-        if ($match_locnname)
-        {
-            #
+        if ($match_locnname) {
             # Look through r_location.location_name
-            #
             $stmt = $db->stmt_init();
 
-            $list = $keyword_list;
-            for ($i = 0; $i < count($list); $i++)
-                $list[$i] = "locate(lower('$list[$i]'), lower(L.location_name)) != 0";
-            if ($keyword_join == 'any')
-                $subclause = implode(" or ", $list);
-            else
-                $subclause = implode(" and ", $list);
+            $match_sql = [];
+            $valrefs = [str_repeat('s', count($keyword_list))];
+
+            for ($i = 0; $i < count($keyword_list); $i++) {
+                $match_sql[] = 'locate(?, lower(L.location_name)) != 0';
+                $valrefs[] = &$keyword_list[$i];
+            }
+
+            if ($keyword_join == 'any') {
+                $subclause = implode(" or ", $match_sql);
+            } else {
+                $subclause = implode(" and ", $match_sql);
+            }
 
             $stmt->prepare("
                 select
@@ -67,33 +89,44 @@ function perform_search(&$t)
             ")
                 or dbi_error_trace("prepare failed");
 
+            call_user_func_array(array($stmt, 'bind_param'), $valrefs);
+
             $stmt->execute();
             $stmt->bind_result($state, $location);
 
-            while ($stmt->fetch())
-            {
+            while ($stmt->fetch()) {
                 $url = url_location($state, $location);
-                $context = "";
 
-                $results[] = array($location, $url, "", $context);
+                $rows[] = [
+                    'name' => $location,
+                    'ec_context' => '',
+                    'u_location' => [
+                            'url' => $url,
+                        ],
+                ];
             }
             $stmt->close();
         }
 
-        if ($match_locndesc)
-        {
+        if ($match_locndesc) {
             #
             # Look through r_location_text.text
             #
             $stmt = $db->stmt_init();
 
-            $list = $keyword_list;
-            for ($i = 0; $i < count($list); $i++)
-                $list[$i] = "locate(lower('$list[$i]'), lower(LT.text)) != 0";
-            if ($keyword_join == 'any')
-                $subclause = implode(" or ", $list);
-            else
-                $subclause = implode(" and ", $list);
+            $match_sql = [];
+            $valrefs = [str_repeat('s', count($keyword_list))];
+
+            for ($i = 0; $i < count($keyword_list); $i++) {
+                $match_sql[] = 'locate(?, lower(LT.text)) != 0';
+                $valrefs[] = &$keyword_list[$i];
+            }
+
+            if ($keyword_join == 'any') {
+                $subclause = implode(" or ", $match_sql);
+            } else {
+                $subclause = implode(" and ", $match_sql);
+            }
 
             $stmt->prepare("
                 select
@@ -108,130 +141,128 @@ function perform_search(&$t)
             ")
                 or dbi_error_trace("prepare failed");
 
+            call_user_func_array([$stmt, 'bind_param'], $valrefs);
+
             $stmt->execute();
             $stmt->bind_result($state, $location, $text);
 
-            while ($stmt->fetch())
-            {
+            while ($stmt->fetch()) {
                 $url = url_location($state, $location);
+                $text = highlight_keywords($text, $keyword_list);
 
-                foreach ($keyword_list as $kw)
-                {
-                    $text = preg_replace
-                        (
-                            "/(?!<b>)($kw)(?!<\/b>)/",
-                            '<b>\1</b>',
-                            $text
-                        );
-                }
-                $context = $text;
-
-                $results[] = array($location, $url, "", $context);
+                $rows[] = [
+                    'name' => $location,
+                    'ec_context' => $text,
+                    'u_location' => [
+                            'url' => $url,
+                        ],
+                ];
             }
             $stmt->close();
         }
 
-        if ($match_photo)
-        {
+        if ($match_photo) {
             #
             # Look through r_location_photo.caption
             #
             $stmt = $db->stmt_init();
 
-            $list = $keyword_list;
-            for ($i = 0; $i < count($list); $i++)
-                $list[$i] = "locate(lower('$list[$i]'), lower(LP.caption)) != 0";
-            if ($keyword_join == 'any')
-                $subclause = implode(" or ", $list);
-            else
-                $subclause = implode(" and ", $list);
+            $match_sql = [];
+            $valrefs = [str_repeat('s', count($keyword_list))];
+
+            for ($i = 0; $i < count($keyword_list); $i++) {
+                $match_sql[] = 'locate(?, lower(LP.caption)) != 0';
+                $valrefs[] = &$keyword_list[$i];
+            }
+
+            if ($keyword_join == 'any') {
+                $subclause = implode(" or ", $match_sql);
+            } else {
+                $subclause = implode(" and ", $match_sql);
+            }
 
             $stmt->prepare("
                 select
                     LP.location_state,
                     LP.location_name,
                     LP.seqno,
-                    LP.caption
+                    LP.file,
+                    LP.daterange,
+                    LP.day,
+                    LP.month,
+                    LP.year,
+                    LP.caption,
+                    LP.owner_uid,
+                    IFNULL(U.fullname, IFNULL(LP.legacy_owner, 'Rolfe Bozier')) as fullname
                 from
-                    r_location_photo LP
+                    r_location_photo LP left join r_user U on LP.owner_uid = U.uid
                 where
+                    LP.hold is NULL
+                    and
                     ($subclause)
                 limit 201
             ")
                 or dbi_error_trace("prepare failed");
 
+            call_user_func_array([$stmt, 'bind_param'], $valrefs);
+
             $stmt->execute();
-            $stmt->bind_result($state, $location, $seqno, $caption);
+            $stmt->bind_result($state, $location, $seqno, $file, $daterange, $day, $month, $year, $caption, $uid, $fullname);
 
-            while ($stmt->fetch())
-            {
+            while ($stmt->fetch()) {
                 $url = url_location_photo($state, $location, $seqno);
+                $context = highlight_keywords($caption, $keyword_list);
+                $date = date_cpts2html($day, $month, $year, $daterange);
 
-                # XXX: hacky
-                $url .= '" rel="prettyPhoto[iframes]"';
-
-                foreach ($keyword_list as $kw)
-                {
-                    $caption = preg_replace
-                        (
-                            "/(?!<b>)($kw)(?!<\/b>)/",
-                            '<b>\1</b>',
-                            $caption
-                        );
-                }
-                $context = $caption;
-
-                $results[] = array($location, $url, 'rel="prettyPhoto[iframes]"', $context);
+                $rows[] = [
+                    'name' => $location,
+                    'ec_context' => $context,
+                    'u_photo' => [
+                            'photo_img' => "/media/photos/$file",
+                            'date' => $date,
+                            'caption' => $caption,
+                            'uid' => $uid,
+                            'fullname' => $fullname,
+                            'thumb_img' => "/media/photos/thumbnails/$file",
+                        ],
+                ];
             }
             $stmt->close();
         }
     }
 
-    if (count($results) > 0)
-    {
-        uasort($results, 'sort_by_location');
+    $nrows = count($rows);
+    if ($nrows > 0) {
+        uasort($rows, 'sort_by_location');
 
-        $count = 0;
-        foreach ($results as $row)
-        {
-            if ($count > 200)
-            {
-                $t->setCurrentBlock("WARNING");
-                $t->setVariable("MSG", "Too many matches; results have been truncated");
-                $t->parseCurrentBlock();
-                break;
-            }
-
-            $t->setCurrentBlock("RESULTS-LINE");
-            $t->setVariable("REF-URL", $row[1]);
-            if ($row[2])
-            {
-                $t->setVariable("REF-REL", $row[2]);
-                $t->touchBlock("CAMERA-ICON");
-            }
-            $t->setVariable("REF-TEXT", $row[0]);
-            $t->setVariable("CONTEXT", $row[3]);
-            $t->parseCurrentBlock();
-            $count++;
+        if ($nrows > 200) {
+            $tp['opt_warning'] = "Too many matches; results have been truncated";
+            array_splice($rows, 200);
         }
-    }
-    else
-    {
-        $t->setCurrentBlock("WARNING");
-        $t->setVariable("MSG", "No results matched the search criteria.");
-        $t->parseCurrentBlock();
+
+        $tp['opt_results'] = $rows;
+    } else {
+        $tp['opt_warning'] = "No results matched the search criteria.";
     }
 
-    $t->touchBlock("RESULTS");
-
+    return $tp;
 }
 
-function sort_by_location($a, $b)
-{
-    if ($a[0] == $b[0])
-        return 0;
-    else
-        return $a[0] < $b[0] ? -1 : 1;
+$title = "Text Search";
+
+$tp = [
+    'title' => $title
+];
+
+$searchmode = quote_external(get_post("searchmode"));
+
+if ($searchmode) {
+    $tp = perform_search($tp);
 }
+
+$latte = new Latte\Engine;
+display_page($title, $latte->renderToString('text.latte', $tp));
+
+return;
 
 ?>
